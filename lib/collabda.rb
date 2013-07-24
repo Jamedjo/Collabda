cd62489168a8f51df3a242a4b7a37aec49cf08ca
James <git@jamedjo.co.uk>
2013-07-24 02:20:14 +0100

    Source fetched over net http

--- lib/collabda.rb
+++ lib/collabda.rb
@@ -1,6 +1,8 @@
 require 'json'
+require 'net/http'
+
 module Collabda
-  class Document
+  module Document
     def from_json
 
     end
@@ -12,9 +14,26 @@ module Collabda
       end
     end
 
+    def source(uri)
+      @@source_uri = uri
+      result = Net::HTTP.get(URI.parse(uri))
+    end
+
     private
-    def json_to_hash(json)
+    def self.json_to_hash(json)
       Hash[JSON.parse(json).map{|key,value| [key.to_sym, value]}]
     end
+  end
+end
+
+class TestDoc
+  extend Collabda::Document
+  source "http://data.uniunits.co.uk/units/IIJ.json"
+  property :title
+  property :description
+
+  def initialize
+    @title = "test"
+    @description = "this is a test"
   end
 end
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -5,3 +5,10 @@ describe Collabda::Document do
     subject.send(:json_to_hash,'{"title":"test"}').should eq({:title=>"test"})
   end
 end
+
+describe TestDoc do
+  it "gives access to its properties" do
+    subject.title.should eq "test"
+    subject.description.should eq "this is a test"
+  end
+end
