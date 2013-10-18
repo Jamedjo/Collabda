8397c0498751168d1e40f9de5ef753c6f10448a9
James EJ <james@makersacademy.com>
2013-10-18 16:18:47 +0100

    Yaml data spec cleanup

--- spec/lib/yaml_data_spec.rb
+++ spec/lib/yaml_data_spec.rb
@@ -27,48 +27,44 @@ describe "YamlData model" do
     end
   end
 
-  specify "yaml_source sets a yaml_path class variable" do
-    TestModel = new_test_class.call
-    expect(TestModel.yaml_path).to be_a String
-  end
-
-  specify "yaml_attributes sets fields to use" do
-    model_class = new_test_class.call
-    expect(model_class.instance_variable_get(:@yaml_attributes)).to eq [:name, :description]
-  end
-
-  it "opens specified file on initialize" do
+  it "opens specified file during class definition" do
     File.should_receive(:open)
-    TestModel2 = new_test_class.call
-  end
-
-  it "loads yaml data from file" do
-    TestModel3 = new_test_class.call
-    expect(TestModel3.yaml_data.count).to eq 3
-  end
-
-  it "uses symbols as attribute keys for consistency" do
     model_class = new_test_class.call
-    expect(model_class.yaml_data.first.keys.first).to be_a Symbol
   end
 
-  it "builds a new model for each element in the data" do
-    TestModel4 = new_test_class.call
-    TestModel4.should_receive(:new).exactly(3).times.and_return(double(:test))
-    TestModel4.reload
-  end
+  describe "after class definition" do
+    let!(:model_class){new_test_class.call}
+    specify "yaml_source sets a yaml_path class variable" do
+      expect(model_class.yaml_path).to be_a String
+    end
 
-  it "it provides access to all models" do
-    TestModel5 = new_test_class.call
-    TestModel5.reload
-    expect(TestModel5.all.count).to eq 3
-    expect(TestModel5.all.first.class).to eq TestModel5
-  end
+    specify "yaml_attributes sets fields to use" do
+      expect(model_class.instance_variable_get(:@yaml_attributes)).to eq [:name, :description]
+    end
 
-  it "sets instance variables for each yaml attribute" do
-    model_class = new_test_class.call
-    model_class.reload
-    expect(model_class.all.first.instance_variable_get(:@name)).to eq "Foo"
-  end
+    it "loads yaml data from file" do
+      expect(model_class.yaml_data.count).to eq 3
+    end
 
+    it "uses symbols as attribute keys for consistency" do
+      expect(model_class.yaml_data.first.keys.first).to be_a Symbol
+    end
+
+    it "builds a new model for each element in the data" do
+      model_class.should_receive(:new).exactly(3).times.and_return(double(:test))
+      model_class.reload
+    end
+
+    describe "after data load" do
+      before(:each){model_class.reload}
+      it "it provides access to all models" do
+        expect(model_class.all.count).to eq 3
+        expect(model_class.all.first.class).to eq model_class
+      end
+
+      it "sets instance variables for each yaml attribute" do
+        expect(model_class.all.first.instance_variable_get(:@name)).to eq "Foo"
+      end
+    end
+  end
 end
